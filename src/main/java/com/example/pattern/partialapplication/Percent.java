0adae45659ca480f90ab71d87936e3e7d8dfc60e
kuldeep <kuldeepyadav7291@gmail.com>
2020-12-30 01:36:15 +0530

    Add one example for partial application using lambdas

---
+++ src/main/java/com/example/pattern/partialapplication/Percent.java
@@ -0,0 +1,11 @@
+package com.example.pattern.partialapplication;
+
+import lombok.Value;
+
+/**
+ * @author <a href="kuldeepyadav7291@gmail.com">Kuldeep</a>
+ */
+@Value(staticConstructor = "of")
+class Percent {
+    Double value;
+}
---
+++ src/main/java/com/example/pattern/partialapplication/Sales.java
@@ -0,0 +1,90 @@
+package com.example.pattern.partialapplication;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+/**
+ * Represent a function which will consume {@link Percent} and calculate tax.
+ *
+ * <p>This interface tries the partial application concept of functional programming, this is
+ * not straight forward implementation to understand, so here is the explanation</p>
+ *
+ * @author <a href="kuldeepyadav7291@gmail.com">Kuldeep</a>
+ */
+public interface Sales {
+    Double calculateTax(Percent percent);
+
+    /**
+     * Represent a function which will consume <B>Month Code<B/> i.e <B>JAN<B/> and {@link Percent} and produces total
+     * tax. This is supposed to be used internally only.
+     */
+    interface X {
+        /**
+         * Applies this function to given arguments.
+         * @param toMonth Month code
+         * @param percent Percent tax value
+         * @return Total tax
+         */
+        Double calculateTax(String toMonth, Percent percent);
+
+        /**
+         * Static factory method which will return {@link Sales}
+         * @param toMonth Month Code
+         * @return {@link Sales}
+         */
+        default Sales to(String toMonth) {
+            return percent -> calculateTax(toMonth, percent);
+        }
+    }
+
+    /**
+     * Static factory method which will return {@link X}
+     * @param fromMonth Month code
+     * @return {@link X}
+     */
+    static X from(String fromMonth) {
+        return (toMonth, percent) -> {
+            final Path path = Paths.get("data/sales.txt");
+            try (Stream<String> lines = Files.lines(path)) {
+                Map<String, Integer> monthOrder = new HashMap<>(12);
+                monthOrder.put("JAN", 1);
+                monthOrder.put("FEB", 2);
+                monthOrder.put("MAR", 3);
+                monthOrder.put("APR", 4);
+                monthOrder.put("MAY", 5);
+                monthOrder.put("JUN", 6);
+                monthOrder.put("JUL", 7);
+                monthOrder.put("AUG", 8);
+                monthOrder.put("SEP", 9);
+                monthOrder.put("OCT", 10);
+                monthOrder.put("NOV", 11);
+                monthOrder.put("DEC", 12);
+
+                final List<MonthSale> allSales = lines.map(line -> MonthSale.of(
+                        line.substring(0, 3),
+                        Double.parseDouble(line.substring(4)),
+                        monthOrder.get(line.substring(0, 3))
+                )).sorted().collect(toList());
+
+                int from = monthOrder.get(fromMonth);
+                int to = monthOrder.get(toMonth);
+
+                final List<MonthSale> totalSales = allSales.subList(from - 1, to);
+
+                final Double total = totalSales.stream().map(MonthSale::getAmount).reduce(0d, Double::sum);
+                return total * (percent.getValue()/100.0);
+
+            } catch (IOException ex) {
+                throw new IllegalArgumentException();
+            }
+        };
+    }
+}
