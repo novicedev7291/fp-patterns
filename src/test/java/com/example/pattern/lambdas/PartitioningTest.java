aa53c704c838be33f15c64e5b90aadaa05d56869
kuldeep <kuldeep.yadav@noosyntech.in>
2020-12-29 15:35:21 +0530

    Add Partitioning and sliding window using IntStream

---
+++ src/test/java/com/example/pattern/lambdas/PartitioningTest.java
@@ -0,0 +1,48 @@
+package com.example.pattern.lambdas;
+
+import org.assertj.core.api.BDDAssertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static java.util.Arrays.asList;
+import static java.util.Collections.singletonList;
+import static java.util.stream.Collectors.toList;
+
+/**
+ * @author <a href="kuldeepyadav7291@gmail.com">Kuldeep</a>
+ */
+class PartitioningTest {
+    private final String sentence = "Developers unfamiliar with the technique will have a harder time working with code that uses currying";
+
+    @Test
+    void shouldPartitionSentenceIntoSublistOfWordsOfNonDecreasingLength() {
+        List<String> words = asList(sentence.split(" +"));
+        int SIZE = words.size();
+        final List<Integer> breaks = IntStream.range(1, SIZE)
+                                               .filter(i -> words.get(i-1).length() > words.get(i).length())
+                                               .boxed()
+                                               .collect(toList());
+        breaks.add(0,0);
+        breaks.add(words.size());
+
+
+        List<List<String>> result = IntStream.range(0, breaks.size()-1)
+                .mapToObj(i -> words.subList(breaks.get(i), breaks.get(i+1)))
+                .collect(toList());
+
+        BDDAssertions.assertThat(result).containsExactlyInAnyOrder(
+                asList("Developers", "unfamiliar"),
+                singletonList("with"),
+                asList("the", "technique"),
+                asList("will", "have"),
+                asList("a", "harder"),
+                asList("time", "working"),
+                asList("with","code", "that", "uses", "currying")
+        );
+    }
+}
---
+++ src/test/java/com/example/pattern/lambdas/SlidingWindowTest.java
@@ -0,0 +1,32 @@
+package com.example.pattern.lambdas;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
+
+/**
+ * @author <a href="kuldeepyadav7291@gmail.com">Kuldeep</a>
+ */
+class SlidingWindowTest {
+    private final String sentence = "Developers unfamiliar with the technique will have a harder time working with code that uses currying";
+
+    /**
+     * Kind of main function disguise as test function :)
+     */
+    @Test
+    void shouldProduceNSizeWordsListOfSentenceWithSlidingWindowApproach() {
+        int N = 3;
+        final List<String> words = Arrays.asList(sentence.split(" +"));
+        int SIZE = words.size();
+
+        final List<List<String>> result = IntStream.range(0, SIZE - N + 1)
+                                                    .mapToObj(i -> words.subList(i, i + N))
+                                                    .collect(toList());
+
+        result.forEach(System.out::println);
+    }
+}
