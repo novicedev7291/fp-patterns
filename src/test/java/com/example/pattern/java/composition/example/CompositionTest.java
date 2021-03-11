df74472c4b51b897fe6e458035a1c47d857deaa2
kuldeep <kuldeepyadav7291@gmail.com>
2021-03-11 21:12:55 +0530

    Add java composition test

--- src/test/java/com/example/pattern/java/composition/example/CompositionTest.java
+++ src/test/java/com/example/pattern/java/composition/example/CompositionTest.java
@@ -1,10 +1,17 @@
 package com.example.pattern.java.composition.example;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.assertj.core.data.Percentage;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.function.Function;
 
+import static java.util.stream.Collectors.toMap;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
@@ -38,5 +45,34 @@ class CompositionTest {
 
         final Function<Double, Double> mi2KmsConverter = converter.curry(1.60934d);
         assertThat(mi2KmsConverter.apply(20d)).isCloseTo(32.1869d, Percentage.withPercentage(0.001));
+    }
+
+    @Test
+    void shouldProduceEachEmployeeSalary() {
+        List<Employee> employees = new ArrayList<>();
+
+        Map<Integer, Double> salaryMap = new HashMap<>();
+
+        for(Employee employee : employees) {
+            salaryMap.put(employee.getId(), employee.calculateSalary());
+        }
+
+        salaryMap = employees.stream()
+                            .collect(toMap(Employee::getId, Employee::calculateSalary));
+    }
+
+    @Getter
+    @RequiredArgsConstructor
+    class Employee {
+        private final Integer id;
+        private final String name;
+        private final int age;
+        private final Double basic;
+
+        // constructor and getters omitted for brevity
+        public Double calculateSalary() {
+            Double basicPlusHra = basic + basic * 0.10;
+            return  basicPlusHra - basicPlusHra * 0.30;
+        }
     }
 }
